2098a79e969c6b8d0c0e9ac9ca2509cf93bd7e88
Cory Suida <itxdevcory@gmail.com>
2020-03-04 07:53:48 -0500

    corrected db files

---
+++ controllers/burger_controllers.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var router = express.Router();
+
+var bar = require("../models/burger.js");
+
+router.get
+
+router.post
+
+router.put
+
+
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Jimmy');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger'); 
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
